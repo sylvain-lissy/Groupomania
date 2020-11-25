10027766d801c29979f845614eb1a9e087b0e5a3
NeeXuuS <lissy.sylvain@gmail.com>
2020-11-25 15:07:46 +0100

    Add tourcode

--- Frontend/cli-version/src/main.js
+++ Frontend/cli-version/src/main.js
@@ -5,5 +5,6 @@ import store from "./store"
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "sweetalert2"
+import "axios"
 
 createApp(App).use(store).use(router).mount("#app")
